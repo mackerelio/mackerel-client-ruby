c8e749766d3863711d23e9d2b196eecc383bbe2a
hatz48 <hatz48@hatena.ne.jp>
2014-05-13 17:56:46 +0900

    implement post_metrics

--- lib/mackerel/client.rb
+++ lib/mackerel/client.rb
@@ -13,13 +13,26 @@ module Mackerel
       @api_key = args[:mackerel_api_key]
     end
 
-    def get_hosts(service = nil, roles = nil)
-      mackerel = Faraday.new(:url => @origin) do |faraday|
-        faraday.response :logger if ENV['DEBUG']
-        faraday.adapter Faraday.default_adapter
-        faraday.options.params_encoder = Faraday::FlatParamsEncoder
+    def post_metrics(metrics)
+      client = http_client
+
+      response = client.post '/api/v0/tsdb' do |req|
+        req.headers['X-Api-Key'] = @api_key
+        req.headers['Content-Type'] = 'application/json'
+        req.body = metrics.to_json
       end
-      response = mackerel.get '/api/v0/hosts.json' do |req|
+
+      unless response.success?
+        raise "GET /api/v0/tsdb faield: #{response.status}"
+      end
+
+      data = JSON.parse(response.body)
+    end
+
+    def get_hosts(service = nil, roles = nil)
+      client = http_client
+
+      response = client.get '/api/v0/hosts.json' do |req|
         req.headers['X-Api-Key'] = @api_key
         req.params['service']    = service if service
         req.params['role']       = roles   if roles
@@ -31,6 +44,16 @@ module Mackerel
 
       data = JSON.parse(response.body)
       data['hosts']
+    end
+
+    private
+
+    def http_client
+      Faraday.new(:url => @origin) do |faraday|
+        faraday.response :logger if ENV['DEBUG']
+        faraday.adapter Faraday.default_adapter
+        faraday.options.params_encoder = Faraday::FlatParamsEncoder
+      end
     end
 
   end
