e835a1df8ea1d39649efedbf31247eb06d6a4936
aereal <aereal@aereal.org>
2016-03-11 12:40:31 +0900

    Add Mackerel::Client::Helper.as_safe_metric_name

---
+++ spec/mackerel/client/helper_spec.rb
@@ -0,0 +1,10 @@
+require 'mackerel/client/helper'
+
+describe Mackerel::Client::Helper do
+  describe '.as_safe_metric_name' do
+    it 'translates characters that cannot be used as a metric name to safe characters' do
+      name = 'readme.md'
+      expect(Mackerel::Client::Helper.as_safe_metric_name(name)).to eq('readme-md')
+    end
+  end
+end
