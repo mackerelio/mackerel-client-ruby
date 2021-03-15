917904e0a7338d8bccefac35e031132fa354cf23
myoan <motoki.yoan@gmail.com>
2021-03-15 15:14:19 +0900

    Implements downtime API

---
+++ lib/mackerel/downtime.rb
@@ -0,0 +1,27 @@
+module Mackerel
+  module REST
+    module Downtime
+      def create_downtime(downtime)
+        command = ApiCommand.new(:post, "/api/v0/downtimes", @api_key)
+        command.body = downtime.to_json
+        command.execute(client)
+      end
+
+      def list_downtime()
+        command = ApiCommand.new(:get, "/api/v0/downtimes", @api_key)
+        command.execute(client)
+      end
+
+      def update_downtime(downtime_id, downtime)
+        command = ApiCommand.new(:put, "/api/v0/downtimes/#{downtime_id}", @api_key)
+        command.body = downtime.to_json
+        command.execute(client)
+      end
+
+      def delete_downtime(downtime_id)
+        command = ApiCommand.new(:delete, "/api/v0/downtimes/#{downtime_id}", @api_key)
+        command.execute(client)
+      end
+    end
+  end
+end
